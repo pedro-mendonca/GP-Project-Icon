b48c3d625c7fe9c8238e605c7bdcbafdbe737eb1
pedro-mendonca <ped.gaspar@gmail.com>
2024-09-16 18:58:35 +0100

    Update

--- gp-project-icon.php
+++ gp-project-icon.php
@@ -80,4 +80,4 @@ require_once GP_PROJECT_ICON_DIR_PATH . 'vendor/autoload.php';
 function gp_project_icon_init() {
 	new Project_Icon();
 }
-add_action( 'gp_init', __NAMESPACE__ . '\gp_project_icon_init' );
+// add_action( 'gp_init', __NAMESPACE__ . '\gp_project_icon_init' );
